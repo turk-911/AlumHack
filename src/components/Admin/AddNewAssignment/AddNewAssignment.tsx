04615db702faab8fb7f3fa181b81d69218a7e860
Mokshe Jain <147153672+turk-911@users.noreply.github.com>
2024-09-25 18:44:20 +0530

    Ediyted AddNewAssignment.tsx

--- src/components/Admin/AddNewAssignment/AddNewAssignment.tsx
+++ src/components/Admin/AddNewAssignment/AddNewAssignment.tsx
@@ -4,6 +4,7 @@ const AddNewAssignment: React.FC = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [file, setFile] = useState<File | null>(null);
+  const [pdfLink, setPdfLink] = useState("");
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
       setFile(e.target.files[0]);
@@ -15,20 +16,21 @@ const AddNewAssignment: React.FC = () => {
   return (
     <div className="upload-assignments">
       <div className="inner-div">
-        <h2>Add new Comet ☄️</h2>
+        <h2>Add new PDF ☄️</h2>
         <input
           type="text"
-          placeholder="Assignment Title"
+          placeholder="Enter subject name"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
-        <textarea
+        <input type="text" placeholder="Paste pdf link here" value={pdfLink} onChange={(e) => setPdfLink(e.target.value)}/>
+        {/* <textarea
           placeholder="Assignment Description"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
-        />
+        /> */}
         <input type="file" onChange={handleFileChange} />
-        <button onClick={handleSubmit}>Launch Comet ☄️</button>
+        <button onClick={handleSubmit}>Add PDF ☄️</button>
       </div>
     </div>
   );
